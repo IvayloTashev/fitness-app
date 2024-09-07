694c60fd7f097ef738e23ca3943f059602bcb184
IvayloTashev <i.tashev@abv.bg>
2024-09-07 14:00:57 +0300

    Add HeroBanner component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,18 @@
+import { useState } from "react";
+import { Box } from "@mui/material";
+
+import HeroBaner from "../components/HeroBaner";
+import SearchExercises from "../components/SearchExercises";
+import Exercises from "../components/Exercises";
+
+
+
 export default function Home() {
-   return (
-       <div>
-       Home
-       </div>
-   );
+    return (
+        <Box>
+            <HeroBaner />
+            <SearchExercises />
+            <Exercises />
+        </Box>
+    );
 }
