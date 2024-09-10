59f73e92aff04043f7515ca1e10d7d6c00f58b79
IvayloTashev <i.tashev@abv.bg>
2024-09-10 19:53:54 +0300

    Fix Gome component promerties

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -22,7 +22,7 @@ export default function Home() {
             <Exercises
                 setExercises={setExercises}
                 bodyPart={bodyPart}
-                setBodyPart={setBodyPart}
+                exercises={exercises}
             />
         </Box>
     );
