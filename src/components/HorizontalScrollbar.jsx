3dac35d5ff375a0c4ba9ef2b80e7367968246f89
IvayloTashev <i.tashev@abv.bg>
2024-09-10 19:16:35 +0300

    Add HorizontalScrollbar component

---
+++ src/components/HorizontalScrollbar.jsx
@@ -0,0 +1,33 @@
+import { useContext } from "react";
+import { Box, Typography } from "@mui/material";
+import { ScrollMenu, VisibilityContext } from 'react-horizontal-scrolling-menu'
+
+import BodyPart from "./BodyPart";
+
+import RightArrowIcon from '../assets/icons/right-arrow.png';
+import LeftArrowIcon from '../assets/icons/left-arrow.png';
+
+export default function HorizontalScrollbar({
+    data,
+    bodyPart,
+    setBodyPart
+}) {
+    return (
+        <ScrollMenu>
+            {data.map((item) => (
+                <Box
+                    key={item.id || item}
+                    itemId={item.id || item}
+                    title={item.id || item}
+                    m='0 40px'
+                >
+                    <BodyPart
+                        item={item}
+                        bodyPart={bodyPart}
+                        setBodyPart={setBodyPart}
+                    />
+                </Box>
+            ))}
+        </ScrollMenu>
+    );
+}
