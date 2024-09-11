48bc8ea3b0b971ff29b55e59be4a6e19933195a8
IvayloTashev <i.tashev@abv.bg>
2024-09-11 20:38:36 +0300

    Add Exercises component

--- src/components/Exercises.jsx
+++ src/components/Exercises.jsx
@@ -1,5 +1,35 @@
-export default function Exercises() {
+import { useState, useEffect } from "react";
+import Pagination from "@mui/material/Pagination";
+import { Box, Stack, Typography } from "@mui/material";
+
+import { exerciseOptions, fetchData } from "../utils/fetchData";
+
+export default function Exercises({
+    exercises,
+    setExercises,
+    bodyPart
+}) {
     return (
-        <>Exercises</>
+        <Box
+            id='exercises'
+            sx={{ mt: { lg: '110px' } }}
+            mt='50px'
+            p='20px'
+        >
+            <Typography variant="h3" mb='46px'>
+                Showing Results
+            </Typography>
+            <Stack
+                direction='row'
+                sx={{ gap: { lg: '110px', xs: '50px' } }}
+                flexWrap='wrap'
+                justifyContent='center'
+            >
+                {exercises.map((exercise, index) => (
+                    <p>{exercise.name}</p>
+                ))}
+            </Stack>
+
+        </Box>
     );
 }
