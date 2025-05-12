539829fbe77ab958211cc79add1ce01658e9f718
IvayloTashev <i.tashev@abv.bg>
2025-05-12 20:44:25 +0300

    Add detail component

--- src/pages/ExerciseDetail.jsx
+++ src/pages/ExerciseDetail.jsx
@@ -15,7 +15,7 @@ export default function ExerciseDetail() {
         const exerciseDbUrl = 'https://exercisedb.p.rapidapi.com';
         const youtubeSearchUrl = 'https://youtube-search-and-download.p.rapidapi.com';
 
-        const exerciseDetailData = await fetchData(`${exerciseDbUrl}/exercises/${id}`, exerciseOptions);
+        const exerciseDetailData = await fetchData(`${exerciseDbUrl}/exercises/exercise/${id}`, exerciseOptions);
         setExerciseDetail(exerciseDetailData);
     }
 
@@ -24,7 +24,7 @@ export default function ExerciseDetail() {
 
   return (
     <Box>
-      <Detail ExerciseDetail={exerciseDetail} />
+      <Detail exerciseDetail={exerciseDetail} />
       <ExerciseVideos />
       <SimilarExercises />
     </Box>
